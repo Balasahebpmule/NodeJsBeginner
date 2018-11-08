e6136aafeeecd83b8edd6793136c7c4790246510
Balasaheb Mule <mulebalasaheb@gmail.com>
2018-11-08 15:11:25 +0530

    Upload files with formidable
    Upload files with formidable

--- requestHandler.js
+++ requestHandler.js
@@ -1,17 +1,20 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring"),
+fs = require("fs"),
+util = require("util"),
+formidable = require("formidable");
 
-var querystring = require("querystring");
-function start(response, postData) {
+function start(response) {
     console.log("Request handler 'start' was called.");
     var body = '<html>'+
     '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -19,12 +22,64 @@ function start(response, postData) {
     response.write(body);
     response.end();
 }
-function upload(response, postData) {
+
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent the text: "+
-    querystring.parse(postData).text);
-    response.end();
+   
+    console.log("about to parse");
+   if (request.url == '/upload' && request.method.toLowerCase() == 'post') {
+     
+   var form = new formidable.IncomingForm();
+    console.log("about to parse");
+
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+ var oldpath = files.upload.path;
+     var newpath = 'C:/Users/balasaheb.mule/test.png';
+//"D:/My tasks/Node learnings/First Node/tmp/test.png"; //
+
+        fs.rename(oldpath, newpath, function(error) {
+            if (error) {
+                fs.unlink(newpath);
+                fs.rename(oldpath, newpath);
+            }
+        });
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("Received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end(util.inspect({fields: fields, files: files}));
+        //sponse.end();
+    });
+
+ 
+    return;
+  }
+  // show a file upload form
+  response.writeHead(200, {'content-type': 'text/html'});
+  response.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+} 
+function show(response) {
+    console.log("Request handler 'show' was called.");
+      var newpath = 'C:/Users/balasaheb.mule/test.png';
+//"D:/My tasks/Node learnings/First Node/tmp/test.png"; //
+    fs.readFile(newpath, "binary", function(error, file) {
+        if(error) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
 }
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
